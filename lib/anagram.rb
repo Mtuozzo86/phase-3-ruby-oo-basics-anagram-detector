6e71cae2fa2e6d1fa866df230a240663a3347c16
Mtuozzo86 <Mtuozzo86@gmail.com>
2021-11-05 07:27:51 -0400

    commit tree 7681520fcce69be4465d3da4a0655f7999af9588

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(arr)
+    arr.select do |arr_word|
+      arr_word.chars.sort == @word.chars.sort
+    end
+  end
+end
